b424a2eaae8c75939ce899012c8a6f68929098b9
Marin322 <santik.987@mail.ru>
2026-01-15 17:49:00 +0500

    Реализована примитивная регистрация и авторизация

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpStatus, Post, HttpCode, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDTO } from 'src/Dto/auth/register.dto';
+import { AuthorizationDTO } from 'src/Dto/auth/authorization.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-}
+
+  @Post('register')
+  @HttpCode(HttpStatus.CREATED)
+  async register(@Body() dto: RegisterDTO) {
+    return this.authService.register(dto);
+  };
+
+  @Post('authorization')
+  @HttpCode(HttpStatus.OK)
+  async authorization(@Body() dto:AuthorizationDTO) {
+    return this.authService.authorization(dto);
+  };
+};
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import { AuthorizationDTO } from 'src/Dto/auth/authorization.dto';
 import { RegisterDTO } from 'src/Dto/auth/register.dto';
 import { PrismaService } from 'src/Prisma.service';
 
@@ -17,8 +18,38 @@ export class AuthService {
             throw new ConflictException('Пользователь уже существует');
         };
 
-        // const user = await this.prisma.user.create({
-            
-        // })
-    }
+        const user = await this.prisma.user.create({
+            data: {
+                phoneNumber: dto.phoneNumber
+            },
+            select: {
+                id: true,
+                lastname: true,
+                firstname: true,
+                phoneNumber: true
+            }
+        });
+
+        return {
+            message: 'Пользователь успешно создан',
+            user
+        };
+    };
+
+    async authorization(dto: AuthorizationDTO) {
+        const existing = await this.prisma.user.findFirst({
+            where: {
+                phoneNumber: dto.phoneNumber
+            }
+        });
+
+        if (!existing) {
+            throw new NotFoundException('Аккаунта с этим номером телефона нет');
+        };
+
+        return {
+            message: 'Успешный вход в аккаунт',
+            existing
+        };
+    };
 }
